e751a3a2c13146e2e4c4a9c7e5784f432a57d4f3
root <root@ip-172-31-26-88.us-west-2.compute.internal>
2018-02-04 23:39:46 +0000

    testing helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+let hits = 1;
+
+const server = http.createServer( (request, response) => {
+  if( request.url == '/')
+  {
+    response.end("Hello World! You are caller number " + hits);
+    hits = hits + 1;
+  }
+  else
+  {
+    response.end();
+  }
+});
+
+server.listen(8124);
+console.log("Listening");
